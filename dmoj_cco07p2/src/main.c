1d3a3a1ed75495674110064b18ff4a1846603cc8
Christopher Poile <cpoile@gmail.com>
2022-12-11 19:41:58 -0500

    dmoj cco07p2 passes

--- dmoj_cco07p2/src/main.c
+++ dmoj_cco07p2/src/main.c
@@ -1,16 +1,19 @@
 #include <inttypes.h>
+#include <malloc.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #define SIZE 6
+#define ARR_SIZE 100000
 
 typedef struct s_node s_node;
-struct s_node {
+struct s_node
+{
     int snowflake[SIZE];
     s_node *next;
 };
 
-bool checkRight(int orig[SIZE], int test[SIZE], int start)
+bool checkRight(int orig[], int test[], int start)
 {
     for (int i = 0; i < SIZE; i++)
     {
@@ -21,7 +24,7 @@ bool checkRight(int orig[SIZE], int test[SIZE], int start)
     return true;
 }
 
-bool checkLeft(int orig[SIZE], int test[SIZE], int start)
+bool checkLeft(int orig[], int test[], int start)
 {
     for (int i = 0; i < SIZE; i++)
     {
@@ -32,8 +35,58 @@ bool checkLeft(int orig[SIZE], int test[SIZE], int start)
     return true;
 }
 
+uint64_t hash(int flake[])
+{
+    uint32_t hash = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        hash += flake[i];
+    }
+    return hash % ARR_SIZE;
+}
+
+bool testSnowflake(int flake[], int newFlake[])
+{
+    // test each starting location
+    for (int k = 0; k < SIZE; k++)
+    {
+        if (checkRight(flake, newFlake, k) || checkLeft(flake, newFlake, k))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool findSnowflake(s_node *flakes[], int test[])
+{
+    uint32_t h = hash(test);
+    s_node *cur = flakes[h];
+    while (cur != NULL)
+    {
+        if (testSnowflake(cur->snowflake, test))
+        {
+            return true;
+        }
+        cur = cur->next;
+    }
+
+    // didn't find it, insert it
+    s_node *node = malloc(sizeof(s_node));
+    for (int i = 0; i < SIZE; i++)
+    {
+        node->snowflake[i] = test[i];
+    }
+
+    node->next = flakes[h];
+    flakes[h] = node;
+
+    return false;
+}
+
 // store snowflakes, or use static in main
-int flakes[100000][SIZE];
+s_node *flakes[ARR_SIZE] = {NULL};
 
 int main()
 {
@@ -43,34 +96,24 @@ int main()
     for (int i = 0; i < n; i++)
     {
         int nf[SIZE];
-        for (int j = 0; j < SIZE; j++) {
-        scanf("%d", &nf[j]);
-        }
-
-        // for each existing flake
-        for (int j = 0; j < i; j++)
-        {
-            // test each starting location
-            for (int k = 0; k < SIZE; k++)
-            {
-                if (checkRight(flakes[j], nf, k) || checkLeft(flakes[j], nf, k))
-                {
-                    printf("Found an identical snowflake ");
-                    for (int l = 0; l < SIZE; l++) {
-                        printf("%d ", nf[l]);
-                    }
-                    printf("\n");
-                    return 0;
-                }
-            }
-        }
-
-        // track the new snowflake
         for (int j = 0; j < SIZE; j++)
         {
-            flakes[i][j] = nf[j];
+            scanf("%d", &nf[j]);
+        }
+
+        if (findSnowflake(flakes, nf))
+        {
+            // printf("Found an identical snowflake ");
+            // for (int l = 0; l < SIZE; l++)
+            // {
+            //     printf("%d ", nf[l]);
+            // }
+            // printf("\n");
+            puts("Twin snowflakes found.");
+            return 0;
         }
     }
 
-    puts("No two snowflakes are alike.\n");
+    puts("No two snowflakes are alike.");
+    return 0;
 }
